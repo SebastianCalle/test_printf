0bbb5c65f5fb5e326aa7b7774866c3e451051e5a
alzheimeer <fogniebla@hotmail.com>
2019-07-28 12:36:46 -0500

    update print_bin variable

---
+++ print_bin.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_bin - convert decimal to binary.
+ * @arg: A list pointing to char to print.
+ */
+char print_bin(va_list l)
+{
+	int bit[16];
+	char *relleno0;
+	int i = 0, j = 0;
+
+}
