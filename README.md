34d2429b550c21c920b20167115c5c0507450875
alzheimeer <fogniebla@hotmail.com>
2019-07-29 16:06:34 -0500

    task del

--- README.md
+++
@@ -1,2 +0,0 @@
-# test_printf
-printf test
--- print_float.c
+++
@@ -1,8 +0,0 @@
-/**
- * fx - Prints a float.
- * @arg: A list pointing to float to print.
- */
-void fx(va_list arg)
-{
-	printf("%f", (float)va_arg(arg, double));
-}
